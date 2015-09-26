77431c3a756ca93cd4a385237d798d315a5eea36
Annyce Davis <annyce.davis@gmail.com>
2015-09-25 21:58:34 -0400

    [-] removed unused imports

--- app/src/main/java/info/adavis/adeptandroid/VolleySingleton.java
+++ app/src/main/java/info/adavis/adeptandroid/VolleySingleton.java
@@ -1,11 +1,9 @@
 package info.adavis.adeptandroid;
 
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.support.v4.util.LruCache;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
