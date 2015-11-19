d54b154acfe78ade284fa67e581640c69af3e802
Annyce Davis <annyce.davis@gmail.com>
2015-11-19 17:59:29 -0500

    [+] add mockito

--- app/src/test/java/info/adavis/adeptandroid/books/BooksPresenterTest.java
+++ app/src/test/java/info/adavis/adeptandroid/books/BooksPresenterTest.java
@@ -1,24 +1,95 @@
 package info.adavis.adeptandroid.books;
 
+import android.support.annotation.NonNull;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Collections;
 import java.util.List;
 
+import info.adavis.adeptandroid.data.BooksRepository;
 import info.adavis.adeptandroid.models.Book;
 
-import static org.junit.Assert.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
-@SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
 public class BooksPresenterTest {
 
+    @Mock BooksContract.View booksView;
+    @Mock BooksRepository booksRepository;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
-    public void emptyListOfBooksShouldHaveSizeZero() {
-        List<Book> books = Collections.emptyList();
+    public void validPresenterShouldNotBeNull() {
+        BooksPresenter presenter = new BooksPresenter(booksRepository, booksView);
 
-        int result = books.size();
+        assertNotNull(presenter);
+    }
 
-        assertEquals("the size should be 0", 0, result);
+    @Test(expected = NullPointerException.class)
+    public void nullRepositoryShouldThrowException() {
+        BooksPresenter presenter = new BooksPresenter(null, new BooksContractViewStub());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void nullViewShouldThrowException() {
+        BooksPresenter presenter = new BooksPresenter(new BooksRepositoryStub(), null);
+    }
+
+    @Test
+    public void callbackCompletedShouldShowBooks() {
+        BooksContractViewStub view = new BooksContractViewStub();
+        BooksPresenter presenter = new BooksPresenter(new BooksRepositoryStub(), view);
+
+        presenter.initDataSet();
+
+        assertTrue("show books should have been called", view.isShowBooksCalled());
+    }
+
+    @Test
+    public void repositoryShouldTriggerCallback() {
+        BooksRepositoryStub repository = new BooksRepositoryStub();
+        BooksPresenter presenter = new BooksPresenter(repository, new BooksContractViewStub());
+
+        presenter.initDataSet();
+
+        assertTrue("get books should have been called", repository.isGetBooksCalled());
+    }
+
+    public class BooksContractViewStub implements BooksContract.View {
+
+        private boolean showBooksCalled = false;
+
+        @Override
+        public void showBooks(List<Book> books) {
+            showBooksCalled = true;
+        }
+
+        public boolean isShowBooksCalled() {
+            return showBooksCalled;
+        }
+    }
+
+    public class BooksRepositoryStub implements BooksRepository {
+
+        private boolean getBooksCalled = false;
+
+        @Override
+        public void getBooks(@NonNull LoadBooksCallback callback) {
+            callback.onBooksLoaded(Collections.<Book>emptyList());
+            getBooksCalled = true;
+        }
+
+        public boolean isGetBooksCalled() {
+            return getBooksCalled;
+        }
     }
 
 }
