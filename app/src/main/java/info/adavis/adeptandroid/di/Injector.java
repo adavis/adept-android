76aed4c28e4d5e422609ee69852106f85675a7f1
Annyce Davis <annyce.davis@gmail.com>
2016-05-22 15:09:11 -0400

    [+] Adds the Logging Interceptor

--- app/src/main/java/info/adavis/adeptandroid/di/Injector.java
+++ app/src/main/java/info/adavis/adeptandroid/di/Injector.java
@@ -1,9 +1,16 @@
 package info.adavis.adeptandroid.di;
 
+import info.adavis.adeptandroid.BuildConfig;
 import info.adavis.adeptandroid.Constants;
 import info.adavis.adeptandroid.data.BookService;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import timber.log.Timber;
+
+import static okhttp3.logging.HttpLoggingInterceptor.Level.BODY;
+import static okhttp3.logging.HttpLoggingInterceptor.Level.NONE;
 
 /**
  * @author Annyce Davis
@@ -14,8 +21,31 @@ public class Injector
     {
         return new Retrofit.Builder()
                 .baseUrl( baseUrl )
+                .client( provideOkHttpClient() )
                 .addConverterFactory( GsonConverterFactory.create() )
                 .build();
+    }
+
+    private static OkHttpClient provideOkHttpClient ()
+    {
+        return new OkHttpClient.Builder()
+                .addInterceptor( provideHttpLoggingInterceptor() )
+                .build();
+    }
+
+    private static HttpLoggingInterceptor provideHttpLoggingInterceptor ()
+    {
+        HttpLoggingInterceptor httpLoggingInterceptor =
+                new HttpLoggingInterceptor( new HttpLoggingInterceptor.Logger()
+                {
+                    @Override
+                    public void log (String message)
+                    {
+                        Timber.d( message );
+                    }
+                } );
+        httpLoggingInterceptor.setLevel( BuildConfig.DEBUG ? BODY : NONE );
+        return httpLoggingInterceptor;
     }
 
     public static BookService provideBookService ()
