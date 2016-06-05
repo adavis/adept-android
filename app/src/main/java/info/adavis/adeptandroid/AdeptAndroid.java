57955608e4fc6470b10a00ecb0bd0118ca93d7e6
Annyce Davis <annyce.davis@gmail.com>
2016-06-05 19:25:33 -0400

    [+] Caching with OkHttp 3
    * Added method to AdeptAndroid to get the network status
    * Added ACCESS_NETWORK_STATE to the Manifest
    * Created a Network and basic Interceptor for handling Cache-Control
    Header

--- app/src/main/java/info/adavis/adeptandroid/AdeptAndroid.java
+++ app/src/main/java/info/adavis/adeptandroid/AdeptAndroid.java
@@ -1,19 +1,45 @@
 package info.adavis.adeptandroid;
 
 import android.app.Application;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 import timber.log.Timber;
 
 public class AdeptAndroid extends Application {
 
+    private static AdeptAndroid instance;
+
     @Override
-    public void onCreate() {
+    public void onCreate()
+    {
         super.onCreate();
 
-        if (BuildConfig.DEBUG) {
+        instance = this;
+
+        if (BuildConfig.DEBUG)
+        {
             Timber.plant(new Timber.DebugTree());
         }
 
         Timber.i("Creating our Application");
+    }
+
+    public static AdeptAndroid getInstance ()
+    {
+        return instance;
+    }
+
+    public static boolean hasNetwork ()
+    {
+        return instance.checkIfHasNetwork();
+    }
+
+    public boolean checkIfHasNetwork()
+    {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService( Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
     }
 }
--- app/src/main/java/info/adavis/adeptandroid/di/Injector.java
+++ app/src/main/java/info/adavis/adeptandroid/di/Injector.java
@@ -1,9 +1,17 @@
 package info.adavis.adeptandroid.di;
 
+import java.io.File;
+import java.io.IOException;
+
+import info.adavis.adeptandroid.AdeptAndroid;
 import info.adavis.adeptandroid.BuildConfig;
 import info.adavis.adeptandroid.Constants;
 import info.adavis.adeptandroid.data.BookService;
+import okhttp3.Cache;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -17,6 +25,8 @@ import static okhttp3.logging.HttpLoggingInterceptor.Level.NONE;
  */
 public class Injector
 {
+    private static final String CACHE_CONTROL = "Cache-Control";
+
     public static Retrofit provideRetrofit (String baseUrl)
     {
         return new Retrofit.Builder()
@@ -30,7 +40,25 @@ public class Injector
     {
         return new OkHttpClient.Builder()
                 .addInterceptor( provideHttpLoggingInterceptor() )
+                .addInterceptor( provideOfflineCacheInterceptor() )
+                .addNetworkInterceptor( provideRewriteCacheInterceptor() )
+                .cache( provideCache() )
                 .build();
+    }
+
+    private static Cache provideCache ()
+    {
+        Cache cache = null;
+        try
+        {
+            cache = new Cache( new File( AdeptAndroid.getInstance().getCacheDir(), "http" ),
+                               10 * 1024 * 1024 ); // 10 MB
+        }
+        catch (Exception e)
+        {
+            Timber.e( e, "Could not create Cache!" );
+        }
+        return cache;
     }
 
     private static HttpLoggingInterceptor provideHttpLoggingInterceptor ()
@@ -46,6 +74,49 @@ public class Injector
                 } );
         httpLoggingInterceptor.setLevel( BuildConfig.DEBUG ? BODY : NONE );
         return httpLoggingInterceptor;
+    }
+
+    public static Interceptor provideRewriteCacheInterceptor ()
+    {
+        return new Interceptor()
+        {
+            @Override
+            public Response intercept (Chain chain) throws IOException
+            {
+                Response response = chain.proceed( chain.request() );
+
+                // re-write response header to force use of cache
+                return response.newBuilder()
+                        .header( CACHE_CONTROL, "public, max-age=" + 60 * 2 ) // 2 minutes
+                        .build();
+            }
+        };
+    }
+
+    public static Interceptor provideOfflineCacheInterceptor ()
+    {
+        return new Interceptor()
+        {
+            @Override
+            public Response intercept (Chain chain) throws IOException
+            {
+                Request request = chain.request();
+
+                // Add Cache Control only for GET methods
+                if ( request.method().equals( "GET" ) )
+                {
+                    if ( !AdeptAndroid.hasNetwork() )
+                    {
+                        // 1 week stale
+                        request = request.newBuilder()
+                                .header( CACHE_CONTROL, "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7 )
+                                .build();
+                    }
+                }
+
+                return chain.proceed( request );
+            }
+        };
     }
 
     public static BookService provideBookService ()
